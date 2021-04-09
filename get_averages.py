0b0d21d8a62b68eb697d5047e8691c477fda4161
Saad Mazhar <saad.maz2000@gmail.com>
2021-04-09 18:56:02 -0400

    agent 1, agent 2, and averages are finalized,can really only make time improvements

--- get_averages.py
+++ get_averages.py
@@ -6,7 +6,7 @@ def get_average(times):
     agent_1_score_counter = 0
     agent_2_score_counter = 0
     for time in range(times):
-        maze = Maze(50)
+        maze = Maze(50)s
         agent_1_score_counter += agent.agent(maze, 1)
         maze.reset()
         agent_2_score_counter += agent.agent(maze, 2)
@@ -19,4 +19,4 @@ def get_average(times):
         elif time == 19:
             print("done !!!")
 
-    return agent_1_score_counter / times, agent_2_score_counter / times
+    return agent_1_score_counter/times, agent_2_score_counter/times
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import get_averages
 from environment import Maze
 
 if __name__ == '__main__':
-    agent_1_average, agent_2_average = get_averages.get_average()
+    agent_1_average, agent_2_average = get_averages.get_average(20)
     print(f'This is the average for agent 1: {agent_1_average}')
     print(f'This is the average for agent 2: {agent_2_average}')
