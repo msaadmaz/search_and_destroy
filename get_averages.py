66bc78bf9157498004139d8a8af33f25b6b059f2
Saad Mazhar <saad.maz2000@gmail.com>
2021-04-09 23:15:31 -0400

    updated averages

--- get_averages.py
+++ get_averages.py
@@ -5,18 +5,21 @@ from environment import Maze
 def get_average(times):
     agent_1_score_counter = 0
     agent_2_score_counter = 0
+    agent_1_averages = []
+    agent_2_averages = []
+    counter = 0
     for time in range(times):
-        maze = Maze(50)s
-        agent_1_score_counter += agent.agent(maze, 1)
-        maze.reset()
-        agent_2_score_counter += agent.agent(maze, 2)
-        if time == 4:
-            print("quarter of the way there")
-        elif time == 9:
-            print("half way there")
-        elif time == 14:
-            print("3/4 of the way there")
-        elif time == 19:
-            print("done !!!")
+        maze = Maze(50)
+        for time2 in range(times):
+            agent_1_score_counter += agent.agent(maze, 1)
+            maze.reset()
+            agent_2_score_counter += agent.agent(maze, 2)
+            maze.reset()
+        agent_1_averages.append(agent_1_score_counter / times)
+        agent_2_averages.append(agent_2_score_counter / times)
+        counter += 1
+        agent_1_score_counter = 0
+        agent_2_score_counter = 0
+        print(f'Maze {counter} is done')
 
-    return agent_1_score_counter/times, agent_2_score_counter/times
+    return agent_1_averages, agent_2_averages
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import get_averages
 from environment import Maze
 
 if __name__ == '__main__':
-    agent_1_average, agent_2_average = get_averages.get_average(20)
-    print(f'This is the average for agent 1: {agent_1_average}')
-    print(f'This is the average for agent 2: {agent_2_average}')
+    agent_1_averages, agent_2_averages = get_averages.get_average(10)
+    print(f'These are the averages for agent 1: {agent_1_averages}')
+    print(f'This is the averages for agent 2: {agent_2_averages}')
